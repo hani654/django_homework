390e2e29eee971f23922322ee9ef4ba5bbc1960e
hani654 <hani654@naver.com>
2023-09-27 08:46:16 +0900

    로그인, 로그아웃

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView
+)
+from users.serializers import CustomTokenObtainPairSerializer, UserSerializer
+
+# Create your views here.
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"massage": "가입완료"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"massage": f"${serializer.errors}"}, status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class mockView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response("get 요청")
